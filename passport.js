443b9a6c50ad16a12f4fbcfca82c12489ef5fd03
Jared Grippe <jared@deadlyicon.com>
2023-01-05 20:30:27 -0800

    WIP

--- passport.js
+++ passport.js
@@ -1,23 +1,23 @@
 import passport from 'passport'
 import { Strategy as OAuth2Strategy } from 'passport-oauth2'
 
-passport.use(new OAuth2Strategy(
-  {
-    authorizationURL: `${process.env.DATAPAL_ORIGIN}/api/oidc/auth`,
-    tokenURL: `${process.env.DATAPAL_ORIGIN}/api/oidc/token`,
-    clientID: process.env.OAUTH_DATAPAL_ID,
-    clientSecret: process.env.OAUTH_DATAPAL_SECRET,
-    callbackURL: `${process.env.APP_ORIGIN}/auth/callback`,
-    state: true,
-    pkce: true,
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log('🛳 PASSPORT verify func', { accessToken, refreshToken, profile })
-    // User.findOrCreate({ exampleId: profile.id }, function (err, user) {
-    //   return cb(err, user)
-    // })
-  }
-))
+// passport.use(new OAuth2Strategy(
+//   {
+//     authorizationURL: `${process.env.DATAPAL_ORIGIN}/api/oidc/auth`,
+//     tokenURL: `${process.env.DATAPAL_ORIGIN}/api/oidc/token`,
+//     clientID: process.env.OAUTH_DATAPAL_ID,
+//     clientSecret: process.env.OAUTH_DATAPAL_SECRET,
+//     callbackURL: `${process.env.APP_ORIGIN}/auth/callback`,
+//     state: true,
+//     pkce: true,
+//   },
+//   function(accessToken, refreshToken, profile, cb) {
+//     console.log('🛳 PASSPORT verify func', { accessToken, refreshToken, profile })
+//     // User.findOrCreate({ exampleId: profile.id }, function (err, user) {
+//     //   return cb(err, user)
+//     // })
+//   }
+// ))
 
 passport.serializeUser(function(user, cb) {
   process.nextTick(function() {
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,12 @@
-import { readFile } from 'fs/promises'
 import Router from 'express-promise-router'
 
 import authRoutes, { requireAuth } from './auth.js'
-import cartRoutes from './cart.js'
 
 const routes = new Router()
 
-const loadFake = async name =>
-  JSON.parse(
-    await readFile(`${process.env.APP_PATH}/fake/${name}.json`)
-  )
-
-const products = await loadFake('products')
 
 routes.use((req, res, next) => {
-  res.locals.products = products
+  // res.locals.products = products
   next()
 })
 
@@ -24,22 +16,11 @@ routes.get('/', async (req, res) => {
   res.render('pages/home')
 })
 
-routes.use(cartRoutes)
-
 routes.get('/account', requireAuth, async (req, res) => {
   res.render('pages/account', {
     account: res.locals.user,
   })
 })
 
-routes.get('/datapal/select-shipping-address', (req, res, next) => {
-  const redirectUrl = req.datapal.requestDocumentRedirect({
-    documentType: 'shippingAddress',
-    purpose: 'So we can ship you 🍷 😃',
-    read: true,
-    returnTo: req.query.returnTo,
-  })
-  res.redirect(redirectUrl)
-})
 
 export default routes
